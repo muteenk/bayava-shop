e1ecebe0c42ba5426264de0f353cb195e3d3d0f4
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-05-04 13:01:31 +0530

    Add initial files and configurations for Bayava Shop project

---
+++ client/src/Components/Header/MiniNavbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function MiniNavbar() {
+  return (
+    <div className='w-full flex items-center justify-between h-10 bg-slate-200 drop-shadow-lg'>
+        <ul className='flex items-center justify-between w-full px-80'>
+            <li>
+                <Link to="/">Yoga Store</Link>
+            </li>
+            <li>
+                <Link to="/">Auyrveda Store</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default MiniNavbar
