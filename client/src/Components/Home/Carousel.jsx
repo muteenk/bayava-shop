4c4c0ae8a3734950afd4a6a00c5d9a63c35ea093
muteenk <kundangarmuteen@gmail.com>
2024-05-10 02:41:42 +0530

    Updated Carousel

--- client/src/Components/Home/Carousel.jsx
+++ client/src/Components/Home/Carousel.jsx
@@ -12,27 +12,26 @@ const Carousel = ({slides}) => {
         setCurrent(current === 0 ? slides.length - 1 : current - 1)
     }
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrent(current === slides.length - 1 ? 0 : current + 1)
-        }, 5000)
-        return () => clearInterval(interval)})
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setCurrent(current === slides.length - 1 ? 0 : current + 1)
+    //     }, 5000)
+    //     return () => clearInterval(interval)})
 
 
   return (
-    <div className="overflow-hidden h-[88vh] w-screen relative bg-[#222222] z-0">
+    <div className="overflow-hidden h-[88vh] w-full relative z-0">
         <div className='flex relative transition-transform duration-500 ease-out' style={{transform: `translateX(-${current*100}%)`, zIndex:1}}>
             {
             slides.map((slide, index) => (
-                <React.Fragment key={index}>
-                    <img src={slide.img} className=" object-fill opacity-50" alt={slide.alt} />
+                <div key={index} className={`relative min-w-[100vw] h-[88vh] bg-cover bg-center`} style={{background: `url('${slide.img}')`, backgroundPosition: 'center'}}>
                     {
                         (index === current) ? <>
-                            <h1 className='fixed top-[11.5em] w-[30%] animate-fadeUp text-[#ff8a2b] font-crimson-pro font-bold text-[2em]'  style={{left: `${current*100+10}%`}}>{slide.subTitle}</h1>
-                            <h1 className='fixed top-[5em] w-[30%] animate-fadeUp font-crimson font-bold text-[5em] text-[white] drop-shadow-lg' style={{left: `${current*100+10}%`}}>{slide.title}</h1>
+                            <h1 className='absolute top-[11.5em] left-[10%] w-[30%] animate-fadeUp text-[#ff8a2b] font-crimson-pro font-bold text-[2em]'>{slide.subTitle}</h1>
+                            <h1 className='absolute top-[5em] left-[10%] w-[30%] animate-fadeUp font-crimson font-bold text-[5em] text-[white] drop-shadow-lg'>{slide.title}</h1>
                         </> : null
                     }
-                </React.Fragment>
+                </div>
             ))
             }   
         </div>
